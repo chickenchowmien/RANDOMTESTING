04852aa51a459cbc3880368d3b8eca2297ea4ca8
admiralcoder <djale2008@icloud.com>
2018-05-31 20:56:48 -0400

    commit message for hello world

---
+++ src/test/java/com/fourstay/stepdefinitions/testfile.java
@@ -0,0 +1,12 @@
+package com.fourstay.stepdefinitions;
+
+public class testfile {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("hello world ");
+
+	}
+
+}
